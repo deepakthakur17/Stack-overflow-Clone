e89712b342e61d221dceb0ac3f5dfbc875680945
Deepak <deepakthakur1661999@gmail.com>
2023-01-31 20:46:31 +0530

    Saving to Github

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import answerRoutes from './routes/Answers.js'
 
 const app = express();
 dotenv.config();
-app.use(express.json({limit: "30mb", extended: true }))
-app.use(express.urlencoded({limit: "30mb", extended: true }))
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -24,7 +24,7 @@ app.use('/answer', answerRoutes)
 const PORT = process.env.PORT || 5000
 
 const DATABASE_URL = process.env.CONNECTION_URL
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
-    .catch((err) => console.log(err.message))
+    .catch((err) => console.log(err))
