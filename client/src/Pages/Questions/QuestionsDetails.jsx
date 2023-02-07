f7bf9adbc7cabfb817ea4b72d6cfb2f0686a039a
Deepak <deepakthakur1661999@gmail.com>
2023-02-07 11:57:43 +0530

    save to Github

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -71,7 +71,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = "https://my-stack-overflow-app.netlify.app/";
 
     const handlePostAns = (e, answerLength) =>{
         e.preventDefault()
